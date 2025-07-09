8b7a28e345f72e336a7d1dc00bb746e753955178
modellfan <febaschu@aol.com>
2025-07-09 19:35:11 +0200

    clean up

--- src/comms_bms.cpp
+++ src/comms_bms.cpp
@@ -143,11 +143,7 @@ void enable_BMS_monitor()
 //---------------------------------------------------------------------------------------------------------------------------------------------
 void print_debug()
 {
-    extern Contactormanager contactor_manager;
-    contactor_manager.print();
-
-    extern Shunt_ISA_iPace shunt;
-    shunt.print();   
+  
 }
 
 Task print_debug_timer(1000, TASK_FOREVER, &print_debug);
